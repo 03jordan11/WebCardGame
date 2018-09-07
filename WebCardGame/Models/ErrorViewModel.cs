c89bc75312e988d9d11339166cca4593b8867b50
AYOKA\jordan.burnes <jordan.burnes@ayokasystems.com>
2018-09-07 13:25:10 -0500

    Initial Commit

---
+++ WebCardGame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCardGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
