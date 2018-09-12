084bf6db6e022ae7ead3d6ef5553874a71eee76c
MAT-TING\MAT-TING <jbt296@yahoo.com>
2018-09-11 21:14:37 -0500

    #002 added controller model view

---
+++ WebCardGame/Controllers/OhNoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCardGame.Domains;
+using WebCardGame.Models;
+
+namespace WebCardGame.Controllers
+{
+	public class OhNoController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var game = new Game() {
+				Name = "Oh No 99!",
+				User = new User(),
+				Bots = new List<AI>(),
+				TurnOrder = new List<Player>(),
+				CardStack = new Deck(),
+				DrawDeck = new Deck(),
+				DECKSIZE = 52,
+				MAXHAND = 4,
+				Discard = null,
+			};
+
+			var x = new OhNoViewModel() {
+				OhNoGame = game
+			};
+			return View("Index", x);
+		}
+	}
+}
--- WebCardGame/Domains/Player.cs
+++ WebCardGame/Domains/Player.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace WebCardGame.Domains
 {
     public class Player
