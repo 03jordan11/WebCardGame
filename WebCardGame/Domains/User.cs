358c738d6fa1c9bd0b3919c8679a8d355ae50e52
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-11 20:37:59 -0500

    #001 added Player,Uer,AI class

---
+++ WebCardGame/Domains/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WebCardGame.Domains
+{
+    public class User : Player
+    {
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+    }
+}
