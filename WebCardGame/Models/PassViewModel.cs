bd52f51708f3e141d46ceca987f44c93b1f732e9
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-18 11:46:59 -0500

    #003 added cs files for Pass

---
+++ WebCardGame/Models/PassViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCardGame.Domains;
+
+namespace WebCardGame.Models
+{
+    public class PassViewModel
+    {
+        public Game Pass { get; set; }
+
+    }
+}
